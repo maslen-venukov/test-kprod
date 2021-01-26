f30068ac0cb402d900d9f2dc152edeea3d7adb27
Sergei <maslenv@bk.ru>
2021-01-27 03:04:26 +0500

    fixed nav list

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
 const headerBurger = document.querySelector('.header__burger');
 const headerNavList = document.querySelector('.header .nav__list');
+const bodyHtml = document.querySelector('html, body');
 
 headerBurger.addEventListener('click', () => {
   headerBurger.classList.toggle('active');
   headerNavList.classList.toggle('active');
+  bodyHtml.classList.toggle('lock');
 })
